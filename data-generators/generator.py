ad34bca01af3a79398c140412cc8129172939d78
michal <mszablewski2004@gmail.com>
2024-03-02 00:14:37 +0100

    witam

--- data-generators/generator.py
+++ data-generators/generator.py
@@ -1,19 +1,22 @@
 from faker import Faker
 import csv
 import numpy as np
+import uuid
 
 fake = Faker()
 mean_age = 40
-age_sd = 10
+age_sd = 10    
 
-with open('output-data/names.csv', 'w', newline='') as csvfile:
+with open('output-data/people.csv', 'w') as csvfile:
     csv_writer = csv.writer(csvfile)
-    csv_writer.writerow(['First Name', 'Last Name', 'Age'])
+    csv_writer.writerow(['id', 'first_name', 'last_name', 'age'])
     
-    for _ in range(10):
+    for _ in range(1000):
+        id = uuid.uuid4()
+        
         name = fake.name().split()
         
         age = int(np.random.normal(mean_age, age_sd))
         age = max(18, min(age, 65))
         
-        csv_writer.writerow([name[0], name[-1], age])
+        csv_writer.writerow([id, name[0], name[-1], age])
